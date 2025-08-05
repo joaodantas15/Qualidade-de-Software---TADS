f4e378ae1e304cd07d0da4a4af3b0e5ba1dd4023
joaodantas15 <111694094+joaodantas15@users.noreply.github.com>
2025-08-05 22:45:11 +0000

    feat: Adiciona suporte a HTTPS com certificado autoassinado

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
-    "django_extensions",  
+    "django_extensions",
+    "weather_app",   
 ]
 
 MIDDLEWARE = [
